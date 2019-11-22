0ec259cd163502c6d75f6d8b61a10711565190e8
Taurus-JuanP <juanpablo.reyes@cocodrilodog.com>
2019-11-22 18:11:11 -0500

    Boot prefabs are now instantiated in `RuntimeInitializeOnLoadMethod`.

--- Scripts/Editor/BootPrefabsEditor.cs
+++ Scripts/Editor/BootPrefabsEditor.cs
@@ -19,31 +19,13 @@
 	public class BootPrefabsEditor : Editor {
 
 
-		#region Static Event Handlers
-
-		private static void EditorApplication_playModeStateChanged(PlayModeStateChange obj) {
-			switch(obj) {
-				case PlayModeStateChange.ExitingEditMode:
-					if (FindObjectOfType<BootInstantiator>() == null) {
-						InstantiateBootPrefabs();
-					}
-					break;
-				case PlayModeStateChange.EnteredEditMode:
-					if (FindObjectOfType<BootInstantiator>() == null) {
-						DestroyBootprefabs();
-					}
-					break;
-			}
-		}
-
-		#endregion
-
-
 		#region Private Static Methods
 
-		[InitializeOnLoadMethod]
-		private static void InitializeOnLoad() {
-			EditorApplication.playModeStateChanged += EditorApplication_playModeStateChanged;
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		private static void BeforeSceneLoad() {
+			if (FindObjectOfType<BootInstantiator>() == null) {
+				InstantiateBootPrefabs();
+			}
 		}
 
 		private static void InstantiateBootPrefabs() {
@@ -55,35 +37,9 @@
 						// Instantiate each of the prefabs with PrefabUtility to preserve
 						// the connection to the prefab.
 						GameObject original = bootPrefabs.GetPrefabAt(i);
-						GameObject clone = PrefabUtility.InstantiatePrefab(original) as GameObject;
+						GameObject clone = Instantiate<GameObject>(original);
 						Debug.LogFormat("BootPrefabs: Instantiated boot object {0}", clone.name);
 						clone.name = original.name;
-					}
-				}
-			}
-		}
-
-		private static void DestroyBootprefabs() {
-			// Didn't find a BootInstantiator, we need to destroy the created clones
-			foreach (BootPrefabs bootPrefabs in Resources.LoadAll<BootPrefabs>("")) {
-				if (CanRunInActiveScene(bootPrefabs)) {
-					// Iterate through the prefabs
-					for (int i = 0; i < bootPrefabs.PrefabsCount; i++) {
-
-						GameObject prefab = bootPrefabs.GetPrefabAt(i);
-						// Iterate through the game objects in the scene to check if they
-						// are derived from the prefab
-						foreach (GameObject gameObject in FindObjectsOfType<GameObject>()) {
-							if (prefab == PrefabUtility.GetCorrespondingObjectFromSource(gameObject)) {
-								// Destroy the instance. It will only be destroyed if it was instantiated
-								// with the PrefabUtility (when the objects are blue). This ensures that
-								// any other instance created at runtime with the normal Instantiate()
-								// methods, won't be destroyed.
-								Debug.LogFormat("BootPrefabs: Destroyed boot object {0}", gameObject.name);
-								DestroyImmediate(gameObject);
-							}
-						}
-
 					}
 				}
 			}
