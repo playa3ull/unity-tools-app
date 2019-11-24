7e9b1d314bf135d911ac917290780d6b15d8e1c7
Taurus-JuanP <juanpablo.reyes@cocodrilodog.com>
2019-11-24 17:24:12 -0500

    Created `MultiplatformScriptableObject`.

---
+++ Scripts/Editor/MultiplatformScriptableObjectEditor.cs
@@ -0,0 +1,32 @@
+﻿namespace CocodriloDog.App {
+
+	using CocodriloDog.Core;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEditor;
+
+	[CustomEditor(typeof(MultiplatformScriptableObject))]
+	public class MultiplatformScriptableObjectEditor : KeyedResourceEditor {
+
+
+		#region Protected Properties
+
+		protected override float PropertiesMiddle { get { return 0.4f; } }
+
+		protected override string KeyLabel { get { return "Platform"; } }
+
+		protected override float KeyLabelWidth { get { return 55; } }
+
+		protected override string ResourceLabel { get { return "Scriptable Object"; } }
+
+		protected override float ResourceLabelWidth { get { return 110; } }
+
+		protected override string FallbackKeyValue { get { return ((MultiplatformScriptableObject)target).FallbackKey.ToString(); } }
+
+		#endregion
+
+
+	}
+
+}
---
+++ Scripts/MultiplatformScriptableObject.cs
@@ -0,0 +1,31 @@
+﻿namespace CocodriloDog.App {
+
+	using CocodriloDog.Core;
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	[CreateAssetMenu(menuName = "Cocodrilo Dog/App/Multiplatform ScriptableObject")]
+	public class MultiplatformScriptableObject : KeyedResource<RuntimePlatform, ScriptableObject, MultiplatformScriptableObjectEntry> {
+
+
+		#region Public Properties
+
+		public override RuntimePlatform CurrentKey {
+			get { return Application.platform; }
+		}
+
+		public override RuntimePlatform FallbackKey {
+			get { return RuntimePlatform.WindowsPlayer; }
+		}
+
+		#endregion
+
+
+	}
+
+	[Serializable]
+	public class MultiplatformScriptableObjectEntry : KeyedResourceEntry<RuntimePlatform, ScriptableObject> { }
+
+}
