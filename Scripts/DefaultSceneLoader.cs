abbd0bf0513f3ca3ee3db96a48d33a016ec5886a
Juan Pablo Reyes Lega <juanpablo.reyes@cocodrilodog.com>
2020-04-06 17:01:31 -0500

    Added `DefaultSceneLoader`.

---
+++ Scripts/DefaultSceneLoader.cs
@@ -0,0 +1,28 @@
+﻿namespace CocodriloDog.App {
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.SceneManagement;
+
+	public class DefaultSceneLoader : MonoBehaviour {
+
+
+		#region Public Fields
+
+		public string DefaultSceneName;
+
+		#endregion
+
+
+		#region Unity Methods
+
+		private void Start() {
+			SceneLoader.LoadScene(DefaultSceneName, LoadSceneMode.Single);
+		}
+
+		#endregion
+
+
+	}
+}
