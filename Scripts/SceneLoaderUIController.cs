24146c4049bb16f701bb82aa319b3bb6a49e3a56
Juan Pablo Reyes Lega <juanpablo.reyes@cocodrilodog.com>
2020-04-06 16:50:47 -0500

    Added `SceneLoaderUIController`.

---
+++ Scripts/SceneLoaderUIController.cs
@@ -0,0 +1,77 @@
+﻿namespace CocodriloDog.App {
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public class SceneLoaderUIController : AbstractSceneLoaderUIController {
+
+
+		#region Public Properties
+
+		public override CanvasGroup CanvasGroup { get { return m_CanvasGroup; } }
+
+		public override Button ActivateSceneButton { get { return m_ActivateSceneButton; } }
+
+		#endregion
+
+
+		#region Public Methods
+
+		public override void OnFadeIn(float duration) {
+			base.OnFadeIn(duration);
+			if (ActivateSceneButton != null) {
+				ActivateSceneButton.gameObject.SetActive(false);
+			}
+		}
+
+		public override void OnLoadProgress(float progress) {
+			base.OnLoadProgress(progress);
+			ProgressImage.fillAmount = progress / 0.9f;
+		}
+
+		public override void OnLoadComplete() {
+			base.OnLoadComplete();
+			if (ActivateSceneButton != null) {
+				ActivateSceneButton.gameObject.SetActive(true);
+			}
+		}
+
+		#endregion
+
+
+		#region Unity Methods
+
+		private void Start() {
+			if (ActivateSceneButton != null) {
+				ActivateSceneButton.gameObject.SetActive(false);
+			}
+		}
+
+		#endregion
+
+
+		#region Private Fields
+
+		[SerializeField]
+		private CanvasGroup m_CanvasGroup;
+
+		[SerializeField]
+		private Button m_ActivateSceneButton;
+
+		[SerializeField]
+		private Image m_ProgressImage;
+
+		#endregion
+
+
+		#region Public Properties
+
+		private Image ProgressImage { get { return m_ProgressImage; } }
+
+		#endregion
+
+
+	}
+}
