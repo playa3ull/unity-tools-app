5bed824f25fda4269b3c3c926005f7ec3c8aaa2d
Juan Pablo Reyes Lega <juanpablo.reyes@cocodrilodog.com>
2020-12-31 13:43:58 -0500

    Simplified and renamed `AppSettings` -> `DefaultAppSettings`.

---
+++ Scripts/DefaultAppSettings.cs
@@ -0,0 +1,35 @@
+﻿namespace CocodriloDog.App {
+
+	using CocodriloDog.Core;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class DefaultAppSettings : MonoSingleton<DefaultAppSettings> {
+
+
+		#region Public Fields
+
+		[SerializeField]
+		public int TargetFrameRate = 60;
+
+		#endregion
+
+
+		#region Unity Methods
+
+		protected override void Awake() {
+			base.Awake();
+			DontDestroyOnLoad(gameObject);
+		}
+
+		private void Start() {
+			Application.targetFrameRate = TargetFrameRate;
+		}
+
+		#endregion
+
+
+	}
+
+}
